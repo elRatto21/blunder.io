cdedbe8bb9959f379c8eca74f5d1966b958d1873
Niklas Trapp <niklas031712@gmail.com>
2023-11-15 15:54:27 +0100

    Update logs

--- root/client/src/services/socket.js
+++ root/client/src/services/socket.js
@@ -2,7 +2,13 @@ import { io } from "socket.io-client";
 
 let socket
 if(localStorage.getItem("accessToken")) {
+
+    
+
     const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET + localStorage.getItem("accessToken");
+    
+    console.log("server_url: " + SOCKET_SERVER_URL)
+    
     socket = io(SOCKET_SERVER_URL);
 }
 
