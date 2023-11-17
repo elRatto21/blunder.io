46f4b2717a278eb1d667ea8679d2bf23dff8829b
Niklas Trapp <niklas031712@gmail.com>
2023-11-17 11:58:56 +0100

    add toast for logout

--- root/client/src/components/settings/Settings.js
+++ root/client/src/components/settings/Settings.js
@@ -2,9 +2,12 @@ import React, { useContext } from 'react';
 import { ThemeContext } from '../common/ThemeProvider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun, faUser } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
 
 const SettingsPage = () => {
     const { theme, setTheme } = useContext(ThemeContext);
+    const navigate = useNavigate();
 
     const toggleTheme = () => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
@@ -12,7 +15,18 @@ const SettingsPage = () => {
 
     const logout = () => {
         localStorage.removeItem("accessToken")
-        window.location.href = "/auth/login"
+
+        toast.success("See you next time 🥺", {
+            position: "top-center",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "light",
+          });
+
+        navigate("/auth/login")
     }
 
     return (
