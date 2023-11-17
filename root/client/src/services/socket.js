0b7a7f079864634b54e9119f9a2fdc1a0a3588be
Niklas Trapp <niklas031712@gmail.com>
2023-11-17 08:44:30 +0100

    update socket config

--- root/client/src/services/socket.js
+++ root/client/src/services/socket.js
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
-console.log(process.env.REACT_APP_SOCKET + " " + localStorage.getItem("accessToken"))
-
 let socket
 if(localStorage.getItem("accessToken")) {
-    const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET + localStorage.getItem("accessToken");
-    
+    const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET + "?accessToken=" + localStorage.getItem("accessToken");
     socket = io(SOCKET_SERVER_URL);
 }
 
