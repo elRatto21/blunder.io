b8f1f1e57fbb52357ff24501297666faf51c6657
Niklas Trapp <niklas031712@gmail.com>
2023-11-23 13:14:19 +0100

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ mvn install
 
 cd ../../
 
+cp ./react.env ./root/client/.env
+
 docker build -t blunder/frontend ./root/client/
 
 docker build -t blunder/backend ./root/backend/
-
-docker compose up -d
 
 docker compose up -d
